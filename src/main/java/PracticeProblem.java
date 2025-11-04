04cdda50cd3f44baca0c489e180fa4c414002729
rajeeve-ravi-ics4u1-01-25 <rajeeve.ravi@student.tdsb.on.ca>
2025-11-03 22:15:19 -0500

    Stu submit V1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,57 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+    /**
+     * Sorts parallel arrays by names using selection sort (case-insensitive)
+     * @param names Array of names
+     * @param ages Array of ages (parallel to names)
+     */
+    public static void selectionSortName(String[] names, int[] ages) {
+        int n = names.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (names[j].compareToIgnoreCase(names[minIndex]) < 0) {
+                    minIndex = j;
+                }
+            }
+            // Swap names
+            String tempName = names[i];
+            names[i] = names[minIndex];
+            names[minIndex] = tempName;
 
+            // Swap corresponding ages to keep arrays parallel
+            int tempAge = ages[i];
+            ages[i] = ages[minIndex];
+            ages[minIndex] = tempAge;
+        }
+    }
+
+    /**
+     * Sorts parallel arrays by ages using selection sort
+     * @param names Array of names (parallel to ages)
+     * @param ages Array of ages
+     */
+    public static void selectionSortAge(String[] names, int[] ages) {
+        int n = ages.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (ages[j] < ages[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            // Swap ages
+            int tempAge = ages[i];
+            ages[i] = ages[minIndex];
+            ages[minIndex] = tempAge;
+
+            // Swap corresponding names to keep arrays parallel
+            String tempName = names[i];
+            names[i] = names[minIndex];
+            names[minIndex] = tempName;
+        }
+    }
 	
 
 }
